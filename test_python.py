fc5003a4fb0213f82417b48a2669d4b8fcd4207d
Davor Bratić <davorbratic2@gmail.com>
2023-02-09 20:10:00 +0100

    Change code again!

--- test_python.py
+++ test_python.py
@@ -1,8 +1,8 @@
 someNumber = 10
 
 if(someNumber <= 0):
-    print("Over zerro")
-elif(someNumber > 0):
     print("Bellow zerro")
+elif(someNumber > 0):
+    print("Over zerro")
 else:
     print("zerro")
